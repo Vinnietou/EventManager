6b3e55ad611afe36ecdbbebe069abb34077e9f46
Vinnietou <chojecki.artur@gmail.com>
2020-11-04 23:13:54 +0100

    Added MongoDB Schema and included inGraphQL resolvers enabling creation and querying of the objects

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {graphqlHTTP} = require('express-graphql');
 const {buildSchema} = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
 
@@ -9,12 +12,27 @@ app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHTTP({
     schema: buildSchema(`
+        type Event{
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery{
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation{
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
     
         schema {
@@ -23,19 +41,44 @@ app.use('/graphql', graphqlHTTP({
         }
     `),
     rootValue: {
-        events: () => {
-            return ["Romantic Cooking", "Sailing", "All night coding"];
+        events: async () => {
+            try {
+                return await Event.find();
+            } catch (err) {
+                console.log(err);
+                throw err;
+            }
         },
-        createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+        createEvent: async args => {
+                        
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date)
+            });
+
+            try {
+                const result = await event
+                    .save();
+                console.log(result);
+                return result;
+            } catch (err) {
+                console.log(err);
+                throw err;
+            }
         }
     },
     graphiql: true   
 }));
 
-app.get('/',(req,res,next) => {
-    res.send("Hello World!")
-})
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD
+    }@clustercero.4b7sk.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`,
+    {useNewUrlParser: true, useUnifiedTopology: true}
+    ).then(() => {
+        app.listen(5000);
+        console.log("Database Connection Successful");
+    }).catch(error => {
+        console.log(error);
+    });
 
-app.listen(5000);
