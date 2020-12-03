ad5719a44c3f84ad5dd82cfca1084e8fc7f19d89
Vinnietou <chojecki.artur@gmail.com>
2020-12-03 22:48:52 +0100

    Added a Navbar to the project and some .css styling to the first three pages. Using NavLink for SPA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import React from 'react';
 
-import LoginPage from './components/Login';
-import EventsPage from './components/Events';
-import BookingsPage from './components/Bookings';
+import LoginPage from './components/PageLogin';
+import EventsPage from './components/PageEvents';
+import BookingsPage from './components/PageBookings';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from="/" to="/login" exact />
-        <Route path="/login" component={LoginPage}/>
-        <Route path="/events" component={EventsPage}/>
-        <Route path="/bookings" component={BookingsPage}/>
-      </Switch>
+      <React.Fragment>
+        <MainNavigation />
+        <main className="main-content">
+          <Switch>
+            <Redirect from="/" to="/login" exact />
+            <Route path="/login" component={LoginPage}/>
+            <Route path="/events" component={EventsPage}/>
+            <Route path="/bookings" component={BookingsPage}/>
+          </Switch>
+        </main>
+      </React.Fragment>
     </BrowserRouter>
   );
 }
